381a0e8d4b7c1bf43e3e23949664d0f2209e869d
Joseph Akintunde <joseph1of1@meta.com>
2025-06-05 15:57:27 -0700

    at the modal

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,90 @@
+let data = [
+	{
+		"playlistID": 1,
+		"playlistName": "Late Night Drive",
+		"playlistAuthor": "Melody Haven",
+		"playlistArt": "img/48700121773_3e886b6954_z.jpg",
+		"songs": [
+            "deep thoughts",
+            "mind in awe",
+            "happy!"
+        ]
+	},
+    {
+		"playlistID": 2,
+		"playlistName": "Summer '16",
+		"playlistAuthor": "Groove Guardian",
+		"playlistArt": "img/summer16.jpg",
+		"songs": [
+            "snooze",
+            "cool dune",
+            "holy water"
+        ]
+    },
+    {
+		"playlistID": 3,
+		"playlistName": "Workout Beats",
+		"playlistAuthor": "harmony Herald",
+		"playlistArt": "img/athlete-training-with-dumbbells-in-gym-screaming-royalty-free-image-722227993-1547756570.jpg",
+		"songs": [
+            "go hard at work",
+            "locked",
+            "feelings"
+        ]
+	},
+	{
+		"playlistID": 4,
+		"playlistName": "Road Trip Anthems",
+		"playlistAuthor": "Playlist Pioneer",
+		"playlistArt": "img/ab67616d0000b2735699d396eb1079421c952bb1.jpeg",
+		"songs": [
+            "this is usa",
+            "distance",
+            "shipping love"
+        ]
+	},
+	{
+		"playlistID": 5,
+		"playlistName": "Boss Chick Energy",
+		"playlistAuthor": "Tianna Calafiori",
+		"playlistArt": "img/dino-reichmuth-A5rCN8626Ck-unsplash-scaled.jpeg",
+		"songs": [
+            "Anaconda",
+            "Man Down",
+            "Snake Eyes"
+        ]
+	},
+    {
+        "playlistID": 6,
+		"playlistName": "Rainy Nights",
+		"playlistAuthor": "Tommy Edman",
+        "playlistArt": "img/heart-shape-on-glass-at-rainy-night-yun-han-xu.jpg",
+		"songs": [
+            "2AM in San Jose",
+            "For a Trophy",
+            "Hall of Fame"
+        ]
+    },
+    {
+        "playlistID": 7,
+		"playlistName": "90's R&B",
+		"playlistAuthor": "Bella Mo",
+        "playlistArt": "img/dino-reichmuth-A5rCN8626Ck-unsplash-scaled.jpeg",
+		"songs": [
+            "Amazing",
+            "Beautiful Girl",
+            "Danger"
+        ]
+    },
+    {
+        "playlistID": 8,
+		"playlistName": "San Francisco",
+		"playlistAuthor": "Raúl Jiménez",
+		"playlistArt": "img/heart-shape-on-glass-at-rainy-night-yun-han-xu.jpg",
+		"songs": [
+            "Baja California",
+            "Oh qué sera",
+            "De bi ti rar mas fotós"
+        ]
+    }
+];
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,57 @@
+const loadSongsToHTML = () =>{
+    const holder = document.querySelector("#playlist-cards")
+    for (let dat of data){
+        const songItemCard = createCard(dat)
+        console.log(songItemCard)
+        holder.appendChild(songItemCard)
+    }
+}
+document.addEventListener("DOMContentLoaded", () =>{
+    loadSongsToHTML()
+})
+// const musicBtn = document.querySelectorAll(".music");
+// const span = document.getElementsByClassName("close")[0];
+// musicBtn.forEach(musicButton => {
+//     musicButton.addEventListener("click",() =>{
+//         modal.style.display = "block"
+//     })
+// });
+// const modalContent = document.querySelector("modal-content")
 
+// function openModal(festival) {
+//    document.getElementById('festivalName').innerText = festival.name;
+//    document.getElementById('festivalImage').src = festival.imageUrl;
+//    document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
+//    document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
+//    document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
+//    modal.style.display = "block";
+// }
+
+const createCard = (data) => {
+    const newPlaylistContainer = document.createElement("div")
+    newPlaylistContainer.className = 'music'
+    console.log("loading",newPlaylistContainer)
+        newPlaylistContainer.innerHTML = `
+            <img src = "${data.playlistArt}" class = 'playlist-art'>
+            <h3 class = 'playlist-name'>${data.playlistName}</h3>
+            <h4 class = 'playlist-author'>${data.playlistAuthor}</h4>
+        `
+        newPlaylistContainer.addEventListener('click',createModal(data));
+        return newPlaylistContainer;
+}
+const createModal = (playlist) => {
+    const modal = document.querySelector(".modal")
+    modal
+}
+const modal = document.querySelector("#festivalModal")
+
+        
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// const modal = document.getElementById("festivalModal");
+// modal.onclick = function(songItemCard) {
+//    if (event.target == modal) {
+//       modal.style.display = "block";
+//    }
+// }
